5ea8f0f34fbac54bae40669909e8687b7699d0b7
divyadakshstory <kumarshushant844@gmail.com>
2026-05-23 09:44:22 +0530

    Create sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.yatra24-7.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ]
+}
