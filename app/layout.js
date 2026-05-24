4627828deae69d952d7a6ae02b1cb71499864ad0
divyadakshstory <kumarshushant844@gmail.com>
2026-05-24 18:58:36 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,8 +19,12 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
+   <html lang="en">
+  <head>
+
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    />
+
+  </head>
