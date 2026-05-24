0d8c0a278df7507f50da367d0cf53d9327969603
divyadakshstory <kumarshushant844@gmail.com>
2026-05-24 19:02:36 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,21 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Yatra24x7",
-  description: "Premium Cab Booking Service Bihar",
+  description: "Premium Cab Booking Bihar",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
-
       </head>
 
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
